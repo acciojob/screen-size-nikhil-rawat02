87cf446c0189a7612a15decf51c156389e9ccb0d
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-15 18:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+console.log(window.innerWidth);
+window.addEventListener("resize" , () => {
+	width.innerHTML  = "Width: " + window.innerWidth;
+  height.innerHTML ="Height: " + window.innerHeight;
+	console.log(width , height);
+});
